6a511575f4d89c23d0249e2a971195db8f87fe99
atul <atul.etech2011@gmail.com>
2017-10-01 18:58:59 +0530

    home responsive except first section

--- src/components/flexBox.jsx
+++ src/components/flexBox.jsx
@@ -29,7 +29,7 @@ export default class FlexBox extends React.Component {
     ));
     return(
       <section id ="myproduct" className="p50">
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
             <div className="products-heading col-md-12 wow fade-in-up">
               <h2 className="heading-title text-center">Products</h2>
@@ -37,17 +37,18 @@ export default class FlexBox extends React.Component {
                 <ol className="carousel-indicators">
                   <li data-target="#myCarousel" data-slide-to="1"></li>
                   <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
                 </ol>
                 <div className="carousel-inner">
                   <div className="item active">
                     <ul className="slides">
-                      <li>
+                      <li className="p-a">
                         <div className="col-md-12">
-                          <div className="list-section col-md-6 zero-padding">
+                          <div className="col-xs-7 list-section zero-padding">
                             {FlexBoxHead}
                             <ul className="navigation-tab nav nav-tabs">
                               <li className="active"><a data-toggle="tab" href="#created">FEATURES</a></li>
-                              <li><a data-toggle="tab" href="#give"> SPECIFICATION</a></li>
+                              <li><a data-toggle="tab" href="#give">SPECIFICATION</a></li>
                             </ul>
                             <div className="tab-content">
                               <div id="created" className="tab-pane fade in active">
@@ -58,9 +59,9 @@ export default class FlexBox extends React.Component {
                               </div>
                             </div>
                           </div>
-                          <div className="col-md-5 chart-section">
+                          <div className="col-xs-5 chart-section">
                             <div id="demo-pie-2" className="pie-title-center" data-percent="80"> <span className="pie-value"></span> </div>
-                            <img src={piechart}></img>
+                            <img src={piechart}/>
                           </div>
                         </div>
                       </li>
@@ -68,13 +69,13 @@ export default class FlexBox extends React.Component {
                   </div>
                   <div className="item">
                     <ul className="slides">
-                      <li>
+                      <li className="p-a">
                         <div className="col-md-12">
-                          <div className="col-md-7 company-page-middle zero-padding">
+                          <div className="col-xs-7 company-page-middle zero-padding">
                             {FlexBoxHead}
                             <ul className="navigation-tab nav nav-tabs">
                               <li className="active"><a data-toggle="tab" href="#created">FEATURES</a></li>
-                              <li><a data-toggle="tab" href="#give"> SPECIFICATION</a></li>
+                              <li><a data-toggle="tab" href="#give">SPECIFICATION</a></li>
                             </ul>
                             <div className="tab-content">
                               <div id="created" className="tab-pane fade in active">
@@ -85,9 +86,9 @@ export default class FlexBox extends React.Component {
                               </div>
                             </div>
                           </div>
-                          <div className="col-md-5 chart-section">
+                          <div className="col-xs-5 chart-section">
                             <div id="demo-pie-2" className="pie-title-center" data-percent="80"> <span className="pie-value"></span> </div>
-                            <img src={piechart}></img>
+                            <img src={piechart}/>
                           </div>
                         </div>
                       </li>
@@ -95,13 +96,13 @@ export default class FlexBox extends React.Component {
                   </div>
                   <div className="item">
                     <ul className="slides">
-                      <li>
+                      <li className="p-a">
                         <div className="col-md-12">
-                          <div className="col-md-7 company-page-middle zero-padding">
+                          <div className="col-xs-7 company-page-middle zero-padding">
                             {FlexBoxHead}
                             <ul className="navigation-tab nav nav-tabs">
                               <li className="active"><a data-toggle="tab" href="#created">FEATURES</a></li>
-                              <li><a data-toggle="tab" href="#give"> SPECIFICATION</a></li>
+                              <li><a data-toggle="tab" href="#give">SPECIFICATION</a></li>
                             </ul>
                             <div className="tab-content">
                               <div id="created" className="tab-pane fade in active">
@@ -112,20 +113,20 @@ export default class FlexBox extends React.Component {
                               </div>
                             </div>
                           </div>
-                          <div className="col-md-5 chart-section">
+                          <div className="col-xs-5 chart-section">
                             <div id="demo-pie-2" className="pie-title-center" data-percent="80"> <span className="pie-value"></span> </div>
-                            <img src={piechart}></img>
+                            <img src={piechart}/>
                           </div>
                         </div>
                       </li>
                     </ul>
                   </div>
                 </div>
-                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                <a className="left carousel-control m-l" href="#myCarousel" data-slide="prev">
                   <span className="glyphicon glyphicon-chevron-left"></span>
                   <span className="sr-only">Previous</span>
                 </a>
-                <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                <a className="right carousel-control m-r" href="#myCarousel" data-slide="next">
                   <span className="glyphicon glyphicon-chevron-right"></span>
                   <span className="sr-only">Next</span>
                 </a>
